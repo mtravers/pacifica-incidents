365496f1c64bc3e2cb7e618eca1434784d322ebe
ken restivo <ken@restivo.org>
2014-02-14 17:17:35 -0800

    Fuck instaparse with a rusty pitchfork. Use plain old clojure for gluing the :stuffs together and parsing out disposition.

--- src/incidents/parse.clj
+++ src/incidents/parse.clj
@@ -25,7 +25,7 @@
                  :hdate #(tfmt/parse
                           (tfmt/formatter "MMMM d, yyyy") %)
                  :time #(tfmt/parse
-                          (tfmt/formatters :hour-minute) %)})
+                         (tfmt/formatters :hour-minute) %)})
 
 (defn transform-all
   [t]
@@ -39,51 +39,66 @@
                   t))
 
 
-(comment
 
-  ;; this tests the results and dumps it to output.edn as a hack
-  ;; to pretty-print it because otherwise it's an unreadable mess
-  ;; set up a buffer with /tmp/output.edn as an auto-revert-mode
-  
+(defn munge-rec
+  "Takes a rec which is [[k v]] pairs with the :rec stripped off.
+   Merges it into a map with the :stuff concatenated."
+  [rec]
+    (reduce (fn [acc [k v]]
+              (if (= k :stuff)
+                (merge-with str acc {:stuff v})
+                (assoc acc k v)))
+            {}
+            rec))
 
-  (->> (ip/parse
-        (ip/parser (slurp "resources/ppd.bnf"))
-        (->  "resources/testdata/well-formed.txt"
-             slurp
-             page-delim-hack))
-       transform-all
-       (urepl/massive-spew "/tmp/output.edn"))
+  (comment
 
+    ;; this tests the results and dumps it to output.edn as a hack
+    ;; to pretty-print it because otherwise it's an unreadable mess
+    ;; set up a buffer with /tmp/output.edn as an auto-revert-mode
+    
 
-  ;; debug version
-  (->> (ip/parse
-        (ip/parser (slurp "resources/ppd.bnf"))
-        (->  "resources/testdata/well-formed.txt"
-             slurp
-             page-delim-hack)
-        ;; for debuggging!
-        :total true
-        :unhide :all) 
-       ;;transform-all ;; don't do the transforms becasue the errors choke it.
-       (urepl/massive-spew "/tmp/output.edn"))
-
-  
-  
-
-  ;; fail-o-rama
-  (ip/parse
-   (ip/parser (slurp "resources/ppd.bnf"))
-   (slurp "resources/testdata/poorly-formed.txt"))
-  
-
-  
-  
-  )
+    (->> (ip/parse
+          (ip/parser (slurp "resources/ppd.bnf"))
+          (->  "resources/testdata/well-formed.txt"
+               slurp
+               page-delim-hack))
+         transform-all
+         (urepl/massive-spew "/tmp/output.edn"))
 
 
-(comment
-   (tfmt/show-formatters)
+    ;; debug version
+    (->> (ip/parse
+          (ip/parser (slurp "resources/ppd.bnf"))
+          (->  "resources/testdata/well-formed.txt"
+               slurp
+               page-delim-hack)
+          ;; for debuggging!
+          :total true
+          :unhide :all) 
+         ;;transform-all ;; don't do the transforms becasue the errors choke it.
+         (urepl/massive-spew "/tmp/output.edn"))
+
+    
+    
+
+    ;; fail-o-rama
+    (ip/parse
+     (ip/parser (slurp "resources/ppd.bnf"))
+     (slurp "resources/testdata/poorly-formed.txt"))
+    
+
+    
+    
+    )
 
 
-  
-  )
+  (comment
+    (tfmt/show-formatters)
+
+
+    ;; sample rec
+
+    
+    
+    )
--- test/incidents/parse_test.clj
+++ test/incidents/parse_test.clj
@@ -1,4 +1,5 @@
 (ns incidents.parse-test
+  (:import org.joda.time.DateTime)
   (:require [clojure.test :refer :all]
             [instaparse.core :as ip]
             [incidents.parse :refer :all]))
@@ -16,6 +17,21 @@
               [:A "a" "a" "a" "a"]
               [:B "b" "b"]]]))))
 
+(deftest munge-rec-test
+  (is (= {:stuff
+          "Occurred on Monterey Rd, Pacifica. RP SOUNDS 1051 // REPORTING HIS BROTHER IS 1051 AND VIOLENT// BROTHER IS IN BEDROOM // RP CALLING FROM LIVING RM // NO WEAPONS // RP WILL OPEN DOORFOR OFC'S Disposition: Log Note Only.",
+          :id 140205007,
+          :type "Dist Family",
+          :time (DateTime. "1969-12-31T17:27:00.000-08:00")}
+         (munge-rec
+          [[:time (DateTime. "1970-01-01T01:27:00.000Z")]
+           [:type "Dist Family"]
+           [:id 140205007]
+           [:stuff
+            "Occurred on Monterey Rd, Pacifica. RP SOUNDS 1051 // REPORTING HIS BROTHER IS 1051 AND VIOLENT"]
+           [:stuff
+            "// BROTHER IS IN BEDROOM // RP CALLING FROM LIVING RM // NO WEAPONS // RP WILL OPEN DOOR"]
+           [:stuff "FOR OFC'S Disposition: Log Note Only."]]))))
 
 (comment
 
