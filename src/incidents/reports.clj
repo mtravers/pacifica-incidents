99d8c4391fa2e925b78bec9f22997b4dc2486b4d
ken restivo <ken@restivo.org>
2014-02-17 18:42:47 -0800

    Find broken geos.

--- src/incidents/reports.clj
+++ src/incidents/reports.clj
@@ -85,6 +85,11 @@
   []
   (utils/total-not-null-counts :address))
 
+(defn botchy-geos
+  []
+  (utils/key-set-counts (comp type :geo)))
+
+
 (defn quick-status
   []
   {:total-incidents (total-records)
@@ -92,6 +97,7 @@
    :total-dispositions (disposition-total)
    :total-descriptions (description-total)
    :total-geos (geo-total)
+   :botchy-geos (botchy-geos)
    :total-addresses (total-addresses)
    :min-max-dates   (dates-min-max)})
 
@@ -130,6 +136,15 @@
          (urepl/massive-spew "/tmp/output.edn")))
   
   (urepl/massive-spew "/tmp/output.edn" *1)
+
+
+  ;; DOH! but in geo functions is pushing the address into geo.
+  ;; geocoding, like zen, is pain and suffering.
+  (->> @db/db
+       vals
+       (map :geo)
+       (filter #(= java.lang.String (type %)))
+       (take 10))
   
   )
 
