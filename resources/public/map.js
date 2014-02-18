21af9b0bb9cc1a7d82c49803a40cdb68c03796aa
ken restivo <ken@restivo.org>
2014-02-17 19:13:23 -0800

    Oh jquery, you so cray cray. Also, emacs indent-region went a little bananas, sorry Mike. But it works for larger values of works now.

--- resources/public/map.js
+++ resources/public/map.js
@@ -1,8 +1,8 @@
 function map_init(lat, lng) {
     var myOptions = {
-	zoom: 10,
-	center: new google.maps.LatLng(lat,lng),
-	mapTypeId: google.maps.MapTypeId.ROADMAP
+		zoom: 10,
+		center: new google.maps.LatLng(lat,lng),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
@@ -17,13 +17,13 @@ function map_init(lat, lng) {
 
     // load data
     $.ajax("/api?count=50", {
-//	data: {lat: lat, lng: lng},
-	success:
-	function(response) {
-	    displayIncidents(map, response);
-	},
-	error: 
-	function(XHR, textStatus, errorThrown) {alert("error: " + textStatus + "; " + errorThrown);}
+		//	data: {lat: lat, lng: lng},
+		success:
+		function(response) {
+			displayIncidents(map, response);
+		},
+		error: 
+		function(XHR, textStatus, errorThrown) {alert("error: " + textStatus + "; " + errorThrown);}
     });
 
 }
@@ -32,29 +32,33 @@ function displayIncidents(map, incidents) {
     var z = 1000;
     var w = new google.maps.InfoWindow();
     for (idx in incidents) {
-	var incident = incidents[idx];
-	if ('geo' in incident && null !=== incident.geo) {
-	    var loc = incident.geo.geometry.location;
-	    var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(loc.lat,loc.lng),
-		map: map,
-		zIndex: z--,
-		title: incident.name});
-	    prepMarker(marker, incident, map, w);
-	}
+		var incident = incidents[idx];
+		if ('geo' in incident && null !== incident.geo) {
+			var loc = incident.geo.geometry.location;
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(loc.lat,loc.lng),
+				map: map,
+				zIndex: z--,
+				title: incident.name});
+			prepMarker(marker, incident, map, w);
+		}
     }
 }
 
 // not working at all yet, placeholder +++
 function prepMarker(marker, incident, map, w) {
     google.maps.event.addListener(marker, 'click', function() {
-//	w.setContent(incident.name + '<br/>' + incident.phone);
-	$.ajax("/marker-info", {
-	    data: {m: incident.index},
-	success:
-	function(response) {
-	    w.setContent(response);	    
-	}});
-	w.open(map, marker);
+		//	w.setContent(incident.name + '<br/>' + incident.phone);
+		$.ajax("/marker-info", {
+			data: {m: incident.index},
+			success:
+			function(response) {
+				w.setContent(response);	    
+			}});
+		w.open(map, marker);
     });
 }
+
+
+$(document).ready(function() { map_init(37.6687476,-122.4836863)});
+				   
