d85bd365ee7fd712a25d068efc0592be20383714
Mike Travers <mtravers@parkerici.org>
2021-02-06 22:16:41 -0800

    complete aws api client

--- src/incidents/aws.clj
+++ src/incidents/aws.clj
@@ -8,6 +8,7 @@
             [utilza.log :as ulog]
             [utilza.aws :as uaws]
             [org.parkerici.multitool.core :as u]
+            [org.parkerici.multitool.cljcore :as ju]
             [cognitect.aws.client.api :as aws]
             ))
 
@@ -56,28 +57,63 @@
   [service invoke-options result-key]
   (loop [acc []
          options invoke-options]
-    (print ".")
     (let [res (aws/invoke service options)
           new-acc (concat acc (result-key res))]
+      (prn :status (:JobStatus res) (:NextToken res))
       (when (:ErrorResponse res)
         (throw (ex-info "AWS Call Failed" res)))
-      (if (-> res :NextToken empty?)
-        new-acc
-        (recur new-acc (assoc-in options [:request :NextToken] (:NextToken res)))))))
+      (if (= (:JobStatus res) "SUCCEEDED") ; note: this may be specific to :textract api
+        (if (-> res :NextToken empty?)
+          new-acc
+          (recur new-acc (assoc-in options [:request :NextToken] (:NextToken res))))
+        ;; not ready
+        (do
+          (java.lang.Thread/sleep 1000)
+          (recur acc options))))))
 
 
-(u/defn-memoized client []
-  (aws/client {:api :textract
-                :region "us-west-2"
-                :credentials-provider (credentials/profile-credentials-provider "default")}))
+(u/defn-memoized client [service]
+  (aws/client {:api service
+               :region "us-west-2"
+               :credentials-provider (credentials/profile-credentials-provider "default")}))
 
 (defn job-id->blocks
   [job-id]
   (invoke-paged
-   (client)
+   (client :textract)
    {:op :GetDocumentAnalysis
     :request {:JobId job-id}}
    :Blocks))
 
+(def bucket "incidents")
+
+(defn pdf->s3
+  [file]
+  (let [key (ju/random-uuid)]
+    (with-open [stream (clojure.java.io/input-stream file)]
+      (aws/invoke (client :s3)
+                  {:op :PutObject
+                   :request {:Bucket bucket
+                             :Key key
+                             ;; TODO ;probably not
+                             :Body stream }}))
+    key))
+
+;;; TODO start the job and wait for it to be ready
+(defn parse-pdf
+  [file]
+  (let [key (pdf->s3 file)
+        job-id (:JobId
+                (aws/invoke (client :textract)
+                            {:op :StartDocumentAnalysis
+                             :request {:DocumentLocation {:S3Object {:Bucket bucket
+                                                                     :Name key}}
+                                       :FeatureTypes ["TABLES"]}}))]
+    
+
+    (prn :job-id job-id)
+    (job-id->blocks job-id)))
 
 
+(comment
+  (def b2 (parse-pdf "data/pdfs/01-10-2020.pdf")))
--- src/incidents/ocr.clj
+++ src/incidents/ocr.clj
@@ -25,7 +25,7 @@
   (get-in block [:Relationships 0 :Ids])) ;note: assumes there are no other :Relationships, which seems true
 
 (defn direct
-  "Turns a set of json blocks into a set of trees, with PAGEs as top elements"
+  "Turns a set of json blocks into a set of trees, with PAGEs as top elements and sub-blocks on the :children attribute"
   [blocks]
   (let [block-index (u/index-by :Id blocks)]
     (letfn [(direct-children [b]
@@ -42,10 +42,8 @@
                   (select-keys [:children :Text :ColumnSpan :ColumnIndex :RowSpan :RowIndex :Id :BlockType :Geometry])))]
       (let [direct (map direct-children blocks)]
         ;; Would be easier to just selct for PAGE, but oh well
-        {:pages
-         (filter (fn [b] (not (some (fn [ob] (some (fn [child] (= child b)) (:children ob))) direct)))
-                 direct)
-         :index block-index}))))
+        (filter (fn [b] (not (some (fn [ob] (some (fn [child] (= child b)) (:children ob))) direct)))
+                 direct)))))
 
 ;;; High level parse – turns low-level parse output into blotter entries
 
@@ -60,85 +58,58 @@
        (map cell-contents)))
 
 (defn parse-table
-  [block-index table]
-  (prn :parse-table (count block-index) (count (:children table)))
+  [table]
+  (prn :parse-table (count (:children table)))
   (->> table
        :children
        (group-by :RowIndex)
        (into (sorted-map))
        vals
        (map parse-row)
-       (u/partition-if #(let [id (get % 2)]
-                          (and (not (empty? id))
-                               (u/coerce-numeric-hard id))))))
+       ;; Split on time, it's reliably detected while the id on the right is not
+       (u/partition-if #(re-matches #"^\d\d:\d\d.*" (second %)))))
  
-;;; → multitool
-(defn partition-diff
-  "Partition coll by calling f on adjacent pairs of elements, if true elts are in same partition"
-  [f coll]
-  (loop [partitions []
-         current [(first coll)]
-         tail (rest coll)]
-    (cond (empty? tail) (conj partitions current)
-          (f (last current) (first tail))
-          (recur partitions
-                 (conj current (first tail))
-                 (rest tail))
-          true
-          (recur (conj partitions current)
-                 [(first tail)]
-                 (rest tail)))))
-
 (defn parse-nontable
-  [block-index page-blocks]
-  (prn :parse-nontable (count block-index) (count (:children page-blocks)))
+  [page-blocks]
+  (prn :parse-nontable (count (:children page-blocks)))
   (let [block-top #(get-in % [:Geometry :BoundingBox :Top])
-        lines (partition-diff (fn [a b] (< (- (block-top b) (block-top a)) 0.01))
+        lines (u/partition-diff (fn [a b] (< (- (block-top b) (block-top a)) 0.01))
                               (sort-by block-top (:children page-blocks)))
         items (u/partition-if (fn [line] (u/coerce-numeric-hard (:Text (first line)))))
         ]
     items))
 
 (defn parse-page
-  [block-index page]
+  [page]
   (if-let [table (u/walk-find #(= (:BlockType %) "TABLE") page)]
-    (parse-table block-index table)
-    (parse-nontable block-index page)))
+    (parse-table table)
+    (parse-nontable page)))
 
 (defn entries
-  [{:keys [pages index]}]
-  (mapcat (partial parse-page index) pages))
+  [pages]
+  (mapcat parse-page pages))
 
 (defn parse-entry
   [[line1 line2]]
   (let [[_ time type] (re-matches #"(\d+:\d+)\s+(.*?)" (second line1))
         [_ location disposition] (re-matches #"(?:.*) (?:on|at) (.*) Disposition: (.*)" (second line2))]
-    {:id (nth line1 2)
-     :time time
-     :type type
-     :location location
-     :disposition disposition
-     }))
+    (u/clean-map
+     {:id (nth line1 2)
+      :time time
+      :type type
+      :location location
+      :disposition disposition
+      })))
 
 (comment
 
-(->> ["data/parsed/01-23-2021.json" "data/parsed/01-23-2021a.json"]
-     (map read-file)
-     (mapcat :Blocks)
+;;; Now with AWS API we have all in one file and already list-of-blocks edn form
+(->> "data/parsed/6.edn"
+     slurp
+     read-string
      direct
-     entries)
-
+     entries
      rest
-     (map parse-entry)
-
-
-;;; TODO also should maybe get the date.
-;
-(doseq [f (fs/list-dir "data/parsed")]
-  (prn f)
-  (->> f
-       read-file
-       parse-textract))
-
+     (map parse-entry))
   
 )
